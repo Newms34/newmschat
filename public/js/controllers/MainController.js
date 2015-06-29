843561c796077c91736d67873939dbf14c796a23
Newms34 <Newms3450@msn.com>
2015-06-28 22:14:01 -0400

    disallowed non-standard html

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -26,19 +26,33 @@ app.controller("MainController", function($scope, $window) {
         if (text.indexOf('/wiki ') === 0) {
             var stuffToWiki = text.replace('/wiki ', '');
             text = '<a href="http://en.wikipedia.org/wiki/' + stuffToWiki + '" target="_blank">' + stuffToWiki + '</a>';
+            text = $scope.userName + ': ' + text;
+            socket.emit('chatIn', {
+                chatText: text
+            });
         } else if (text.indexOf('/block ') === 0) {
+            //this and the next 2 if's do not send the data.
             $scope.blockEm(text.replace('/block ', ''), 0);
             return 0;
         } else if (text.indexOf('/unblock ') === 0) {
             $scope.blockEm(text.replace('/unblock ', ''), 1);
             return 0;
-        } else if (text == '') {
+        } else if (text === '') {
             return 0;
+        } else if (text.indexOf('<') != -1 || text.indexOf('>') != -1) {
+            console.log('found some codebits');
+            text = text.replace(/[<]/gi, '&lt;').replace(/[>]/, '&gt;');
+            console.log(text);
+            text = $scope.userName + ': ' + text;
+            socket.emit('chatIn', {
+                chatText: text
+            });
+        } else {
+            text = $scope.userName + ': ' + text;
+            socket.emit('chatIn', {
+                chatText: text
+            });
         }
-        text = $scope.userName + ': ' + text;
-        socket.emit('chatIn', {
-            chatText: text
-        });
         $('#chatInp').val('');
 
     };

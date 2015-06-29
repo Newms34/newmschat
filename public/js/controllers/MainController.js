29825489e593277cd8995f8dffd9a6d057fb3282
Newms34 <Newms3450@msn.com>
2015-06-28 21:14:16 -0400

    block and unblock enabled

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,6 +1,6 @@
 var app = angular.module("Chat", ['ngSanitize']);
-var adjs = ['Insidious', 'Merciful', 'Heavenly', 'Woebegone', 'Victorious', 'Itchy', 'Crooked', 'Wise', 'Wiggly', 'August'];
-var nouns = ['Ladybug', 'Airplane', 'Dog', 'Cat', 'Chipmunk', 'Potato', 'Snail', 'Horse', 'Iguana', 'Pickle'];
+var adjs = ['Insidious', 'Merciful', 'Heavenly', 'Woebegone', 'Victorious', 'Itchy', 'Crooked', 'Wise', 'Wiggly', 'August','Enormous','Fluffy'];
+var nouns = ['Ladybug', 'Airplane', 'Dog', 'Cat', 'Chipmunk', 'Potato', 'Snail', 'Horse', 'Iguana', 'Pickle','Tyrannosaurus','Orangutan','Wallaby','Aardvark','Noodle'];
 
 
 var socket = io();
@@ -9,6 +9,7 @@ app.controller("MainController", function($scope, $window) {
     //on instantiation, create random 'user key'
     // $scope.userKey = Math.floor(Math.random()*999999999999).toString(26);//dont really need this
     $scope.userName = adjs[Math.floor(Math.random() * adjs.length)] + ' ' + nouns[Math.floor(Math.random() * nouns.length)];
+    $scope.blockUser = []
     $scope.chatLines = [{
         txt: '<i>System: Start chatting!</i>',
         id: 0.12345
@@ -21,33 +22,55 @@ app.controller("MainController", function($scope, $window) {
         }
     };
     $scope.speak = function() {
-        var text = $scope.userName + ': ' + $('#chatInp').val();
+        var text = $('#chatInp').val();
+        if (text.indexOf('/wiki ') === 0) {
+            var stuffToWiki = text.replace('/wiki ', '');
+            text = '<a href="http://en.wikipedia.org/wiki/' + stuffToWiki + '" target="_blank">' + stuffToWiki + '</a>';
+        } else if (text.indexOf('/block ') === 0) {
+            $scope.blockEm(text.replace('/block ', ''), 0);
+            return 0;
+        } else if (text.indexOf('/unblock ') === 0) {
+            $scope.blockEm(text.replace('/unblock ', ''), 1);
+            return 0;
+        }
+        text = $scope.userName + ': ' + text;
         socket.emit('chatIn', {
             chatText: text
         });
+
+    };
+    $scope.blockEm = function(text, mode) {
+        if (!mode) {
+            //add user to block list
+            $scope.blockUser.push(text);
+        } else if ($scope.blockUser.indexOf(text) != -1) {
+
+            $scope.blockUser.splice($scope.blockUser.indexOf(text), 1);
+        }
+        $('#chatInp').val('');
+        $scope.$apply();
     };
     socket.on('chatOut', function(text) {
-        if (text.chatText.indexOf($scope.userName + ': /wiki ') === 0) {
-            //user wikied!
-            var stuffToWiki = text.chatText.replace($scope.userName + ': /wiki ','');
-            var wikiText = $scope.userName+ ': <a href="http://en.wikipedia.org/wiki/'+stuffToWiki+'" target="_blank">'+stuffToWiki+'</a>';
-            $scope.chatLines.push({
-                txt: wikiText,
-                id: Math.random()
-            });
-        } else {
+        //first, check to see if user is on block list
+        var foundBlock = false;
+        for (var i = 0; i < $scope.blockUser.length; i++) {
+            if (text.chatText.indexOf($scope.blockUser[i]) === 0) {
+                foundBlock = true;
+            }
+        }
+        if (!foundBlock) {
             $scope.chatLines.push({
                 txt: text.chatText,
                 id: Math.random()
             });
         }
-        $('#chatInp').val('');
-        var chatHeight = 31 * $scope.chatLines.length;
-        $('#chatLog').animate({
-            scrollTop: chatHeight
-        }, 100);
-        $('#chatLog').scrollTop(chatHeight);
-        $scope.$apply();
-        $('#chatLog').focus();
+            $('#chatInp').val('');
+            var chatHeight = 31 * $scope.chatLines.length;
+            $('#chatLog').animate({
+                scrollTop: chatHeight
+            }, 100);
+            $('#chatLog').scrollTop(chatHeight);
+            $scope.$apply();
+            $('#chatLog').focus();
     });
 });

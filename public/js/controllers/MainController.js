81c99e7ebb65f30dbe4ccd1da5a6e9aa01c8b0ea
Newms34 <Newms3450@msn.com>
2015-06-28 20:27:48 -0400

    first of the chat commands. , try /wiki

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,4 +1,4 @@
-var app = angular.module("Chat", []);
+var app = angular.module("Chat", ['ngSanitize']);
 var adjs = ['Insidious', 'Merciful', 'Heavenly', 'Woebegone', 'Victorious', 'Itchy', 'Crooked', 'Wise', 'Wiggly', 'August'];
 var nouns = ['Ladybug', 'Airplane', 'Dog', 'Cat', 'Chipmunk', 'Potato', 'Snail', 'Horse', 'Iguana', 'Pickle'];
 
@@ -10,7 +10,7 @@ app.controller("MainController", function($scope, $window) {
     // $scope.userKey = Math.floor(Math.random()*999999999999).toString(26);//dont really need this
     $scope.userName = adjs[Math.floor(Math.random() * adjs.length)] + ' ' + nouns[Math.floor(Math.random() * nouns.length)];
     $scope.chatLines = [{
-        txt: 'System: Start chatting!',
+        txt: '<i>System: Start chatting!</i>',
         id: 0.12345
     }];
     console.log('generating name');
@@ -27,10 +27,20 @@ app.controller("MainController", function($scope, $window) {
         });
     };
     socket.on('chatOut', function(text) {
-        $scope.chatLines.push({
-            txt: text.chatText,
-            id: Math.random()
-        });
+        if (text.chatText.indexOf($scope.userName + ': /wiki ') === 0) {
+            //user wikied!
+            var stuffToWiki = text.chatText.replace($scope.userName + ': /wiki ','');
+            var wikiText = $scope.userName+ ': <a href="http://en.wikipedia.org/wiki/'+stuffToWiki+'" target="_blank">'+stuffToWiki+'</a>';
+            $scope.chatLines.push({
+                txt: wikiText,
+                id: Math.random()
+            });
+        } else {
+            $scope.chatLines.push({
+                txt: text.chatText,
+                id: Math.random()
+            });
+        }
         $('#chatInp').val('');
         var chatHeight = 31 * $scope.chatLines.length;
         $('#chatLog').animate({
